72e72de8232b151716ed033664d7d128859cb057
Alina <145022081+AlinaN77@users.noreply.github.com>
2023-09-14 16:16:22 +0300

    Add files via upload

---
+++ Program2.cs
@@ -0,0 +1,45 @@
+﻿/*2) Scrie un program care:
+      a) citește de la tastatura o lista de numere (pot fi si numere reale) despărțite prin spațiu și le salvează într-un array
+      b) parcurge array-ul și le afișează pe cele care nu sunt intregi
+      c) cauta si afiseaza cel mai mic numar fara sa folosesti funcții din .NET (Math.Min)*/
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        double[] array = new double[10];
+        int i;
+
+        Console.WriteLine("Introduceti elementele array-ului: ");
+        for (i = 0; i < 10; i++)
+        {
+            Console.Write(" {0} : ", i); 
+            array[i] = Convert.ToDouble(Console.ReadLine());
+        }
+
+        Console.Write("\nElements in array are: ");
+        for (i = 0; i < 10; i++)
+        {
+            Console.Write("{0}  ", array[i]);
+        }
+        Console.Write("\n");
+        Console.WriteLine("Numere reale din array sunt:"); 
+
+        for (i = 0; i < 10; i++)
+        {
+            if (array[i] % 1 != 0)
+                Console.WriteLine(" {0} ", array[i]);
+        }
+
+        double min = array[0];
+        for (i = 0; i < 10; i++)
+        {
+            if (array[i] < min)
+            {
+                min = array[i];
+            }
+        }
+        Console.WriteLine("Minim:" + min); 
+    }
+}
---
+++ Program3.cs
@@ -0,0 +1,54 @@
+﻿/*
+ * 3) Scrie un program care:
+      a) citește de la tastatura 3 nume de persoane (ex. George Ion Maria)
+      b) afișează pe cate o linie ce caracter a apărut în fiecare nume și de cate ori indiferent ca-i cu litera mica sau mare*/
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        string[] names = new string[3];
+
+        for (int i = 0; i < 3; i++)
+        {
+            Console.Write($"Nume {i + 1}: ");
+            names[i] = Console.ReadLine();
+        }
+        Console.WriteLine();
+
+        foreach (string name in names)
+        {
+            foreach (char letter in name)
+            {
+                Console.Write(letter + " ");
+            }
+            Console.WriteLine();
+
+            // Calculăm și afișăm numărul de apariții ale fiecărei litere în nume
+            Console.Write("Numărul de aparitii : ");
+            foreach (char letter in "abcdefghijklmnopqrstuvwxyz")
+            {
+                int count = CountLetter(name.ToLower(), letter);
+                if (count > 0)
+                {
+                    Console.Write(letter + "-" + count + " ");
+                }
+            }
+            Console.WriteLine("\n");
+        }
+    }
+    static int CountLetter(string text, char letter)
+    {
+        int count = 0;
+        foreach (char c in text)
+        {
+            if (c == letter)
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+}
